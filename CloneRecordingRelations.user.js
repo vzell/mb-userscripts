4de788ee1bcda0ae0219d3e7e9e5a0b841938a04
Dr. Volker Zell <vzell@volkerzell.de>
2025-11-30 02:41:29 +0100

    add new scripts

---
+++ CloneRecordingRelations.user.js
@@ -0,0 +1,242 @@
+/* global $ helper MB relEditor requests */
+'use strict';
+// ==UserScript==
+// @name         VZ: MusicBrainz - Clone Recording Relations Onto Other Recordings In Relation Editor
+// @namespace    https://github.com/vzell/mb-userscripts
+// @version      0.9-2025-11-30
+// @author       loujine
+// @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-reledit-clone_relations.user.js
+// @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-reledit-clone_relations.user.js
+// @supportURL   https://github.com/loujine/musicbrainz-scripts
+// @icon         https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/icon.png
+// @description  musicbrainz.org relation editor: Clone recording relations onto other recordings
+// @compatible   firefox+tampermonkey
+// @license      MIT
+// @require      https://gist.githubusercontent.com/reosarevok/e9fc05d7f251379c301b948623b3ef03/raw/e635364e2c3a60578bb349a9a95483711f6c4e4d/gistfile1.js
+// @include      http*://*musicbrainz.org/release/*/edit-relationships
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+const MBID_REGEX = /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/;
+const cloneIdxHelp = `Index(es) to clone relationships from.
+Indexes start at 1 and are related to the list of selected recordings, not the tracklist indexes.
+Indexes can be empty (= 1, first selected), a number n or a range n1-n2.
+Relations are cloned by looping over the 'source' recordings (e.g. '1-2' on 7 selected recordings
+will clone the relations of R1 to R3,R5,R7 and the ones from R2 to R4,R6
+`;
+
+function autoComplete(nodeId, entityType) {
+    const $input = $(`input#${nodeId}`);
+    const match = $input.val().match(MBID_REGEX);
+    if (match) {
+        const mbid = match[0];
+        requests.GET(`/ws/2/${entityType}/${mbid}?fmt=json`, data => {
+            data = JSON.parse(data);
+            $input.data('mbid', mbid);
+            $input.val(data.title || data.name);
+            $input.css('background', '#bbffbb');
+        });
+    } else {
+        $input.data().mbid = "";
+        $input.css('background', '#ffaaaa');
+    }
+}
+
+function autoCompleteRec() {
+    return autoComplete('cloneExtRecording', 'recording');
+}
+
+function autoCompleteRel() {
+    return autoComplete('cloneExtRelease', 'release');
+}
+
+function cloneAR(refIdx) {
+    let startIdx;
+    let range;
+    if (refIdx.includes('-')) {
+        startIdx = parseInt(refIdx.split('-')[0]) - 1;
+        range = parseInt(refIdx.split('-')[1]) - startIdx;
+    } else {
+        startIdx = parseInt(refIdx) - 1 || 0;
+        range = 1;
+    }
+
+    const recordings = relEditor.orderedSelectedRecordings();
+    const sourceRecordings = recordings.splice(startIdx, range);
+
+    recordings.map((rec, idx) => {
+        const sourceRecording = sourceRecordings[idx % sourceRecordings.length];
+        const sourceRels = sourceRecording.relationships.filter(
+            rel => !['recording', 'work'].includes(rel.target_type)
+        );
+        sourceRels.map(sourceRel => {
+            MB.relationshipEditor.dispatch({
+                type: 'update-relationship-state',
+                sourceEntity: rec,
+                ...relEditor.dispatchDefaults,
+                batchSelectionCount: null,
+                newRelationshipState: {
+                    ...relEditor.stateDefaults,
+                    _status: 1,
+                    entity0: sourceRel.backward ? sourceRel.target : rec,
+                    entity1: sourceRel.backward ? rec : sourceRel.target,
+                    entity0_credit: sourceRel.entity0_credit,
+                    entity1_credit: sourceRel.entity1_credit,
+                    begin_date: sourceRel.begin_date,
+                    end_date: sourceRel.end_date,
+                    ended: sourceRel.ended,
+                    attributes: relEditor.createAttributeTree(sourceRel.attributes),
+                    id: MB.relationshipEditor.getRelationshipStateId(),
+                    linkTypeID: sourceRel.linkTypeID,
+                },
+            });
+        });
+    });
+}
+
+function cloneExtAR(recMBID) {
+    if (recMBID.split('/').length > 1) {
+        recMBID = recMBID.split('/')[4];
+    }
+    requests.GET(`/ws/js/entity/${recMBID}?inc=rels`, resp => {
+        const sourceRels = JSON.parse(resp).relationships.filter(
+            rel => rel.target_type != 'work'
+        );
+
+        relEditor.orderedSelectedRecordings().forEach(async (recording, recIdx) => {
+            await helper.delay(recIdx * 100);
+            sourceRels.map(sourceRel => {
+                MB.relationshipEditor.dispatch({
+                    type: 'update-relationship-state',
+                    sourceEntity: recording,
+                    ...relEditor.dispatchDefaults,
+                    batchSelectionCount: null,
+                    newRelationshipState: {
+                        ...relEditor.stateDefaults,
+                        _status: 1,
+                        entity0: sourceRel.backward ? sourceRel.target : recording,
+                        entity1: sourceRel.backward ? recording : sourceRel.target,
+                        entity0_credit: sourceRel.entity0_credit,
+                        entity1_credit: sourceRel.entity1_credit,
+                        begin_date: sourceRel.begin_date,
+                        end_date: sourceRel.end_date,
+                        ended: sourceRel.ended,
+                        attributes: relEditor.createAttributeTree(sourceRel.attributes),
+                        id: MB.relationshipEditor.getRelationshipStateId(),
+                        linkTypeID: sourceRel.linkTypeID,
+                    },
+                });
+            });
+        });
+    });
+}
+
+function cloneReleaseExtAR(relMBID) {
+    if (relMBID.split('/').length > 1) {
+        relMBID = relMBID.split('/')[4];
+    }
+
+    requests.GET(`/ws/js/entity/${relMBID}?inc=rels`, async resp => {
+        const sourceRels = JSON.parse(resp).relationships.filter(
+            rel => rel.target_type !== 'url'
+        );
+        const dialogSource = MB.relationshipEditor.state.entity;
+
+        sourceRels.map(sourceRel => {
+            MB.relationshipEditor.dispatch({
+                type: 'update-relationship-state',
+                sourceEntity: dialogSource,
+                ...relEditor.dispatchDefaults,
+                batchSelectionCount: null,
+                newRelationshipState: {
+                    ...relEditor.stateDefaults,
+                    _status: 1,
+                    entity0: sourceRel.backward ? sourceRel.target : dialogSource,
+                    entity1: sourceRel.backward ? dialogSource : sourceRel.target,
+                    entity0_credit: sourceRel.entity0_credit,
+                    entity1_credit: sourceRel.entity1_credit,
+                    begin_date: sourceRel.begin_date,
+                    end_date: sourceRel.end_date,
+                    ended: sourceRel.ended,
+                    attributes: relEditor.createAttributeTree(sourceRel.attributes),
+                    id: MB.relationshipEditor.getRelationshipStateId(),
+                    linkTypeID: sourceRel.linkTypeID,
+                },
+            });
+        });
+    });
+}
+
+(function displayToolbar() {
+    relEditor.container(document.querySelector('div.tabs'))
+             .insertAdjacentHTML('beforeend', `
+      <details id="clone_rels_script_toggle">
+        <summary style="display: block;margin-left: 8px;cursor: pointer;">
+          <h3 style="display: list-item;">
+            Clone recording relations to selected recordings:
+          </h3>
+        </summary>
+        <div>
+          <span>
+            Source recording index in selection:
+          </span>
+          &nbsp;
+          <input type="text" id="cloneRef" placeholder="empty or n or 'n1-n2'">
+          <span title="${cloneIdxHelp}">🛈</span>
+          &nbsp;
+          <span>OR recording link:&nbsp;</span>
+          <input type="text" id="cloneExtRecording" placeholder="recording mbid">
+          <br />
+          <input type="button" id="cloneAR" value="Apply">
+        </div>
+      </details>
+      <details id="clone_release_rels_script_toggle">
+        <summary style="display: block;margin-left: 8px;cursor: pointer;">
+          <h3 style="display: list-item;">
+            Clone release relations from another release
+          </h3>
+        </summary>
+        <div>
+          <span>release link:&nbsp;</span>
+          <input type="text" id="cloneExtRelease" placeholder="release mbid">
+          <br />
+          <input type="button" id="cloneReleaseAR" value="Apply">
+        </div>
+      </details>
+    `);
+})();
+
+$(document).ready(function () {
+    $('input#cloneExtRecording').on('input', autoCompleteRec);
+    $('input#cloneExtRelease').on('input', autoCompleteRel);
+    $('input#cloneRef').on('input', () => {
+        document.getElementById('cloneExtRecording').value = '';
+        autoCompleteRec();
+    });
+    let appliedNote = false;
+    document.getElementById('cloneAR').addEventListener('click', () => {
+        const recMBID = $('input#cloneExtRecording').data('mbid');
+        if (recMBID) {
+            cloneExtAR(recMBID);
+        } else {
+            const refIdx = document.getElementById('cloneRef').value;
+            cloneAR(refIdx);
+        }
+        if (!appliedNote) {
+            relEditor.editNote(GM_info.script);
+            appliedNote = true;
+        }
+    });
+    document.getElementById('cloneReleaseAR').addEventListener('click', () => {
+        const relMBID = $('input#cloneExtRelease').data('mbid');
+        if (relMBID) {
+            cloneReleaseExtAR(relMBID);
+        }
+        if (!appliedNote) {
+            relEditor.editNote(GM_info.script, 'Cloned from https://musicbrainz.org/release/' + relMBID);
+            appliedNote = true;
+        }
+    });
+    return false;
+});
---
+++ MoveReleaseRelationsToRecordings.user.js
@@ -0,0 +1,73 @@
+/* global $ MB server relEditor */
+'use strict';
+// ==UserScript==
+// @name         MusicBrainz relation editor: Replace release relations by recording relations
+// @namespace    mbz-loujine
+// @author       loujine
+// @version      2024.11.13
+// @downloadURL  https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-reledit-release_rel_to_recording_rel.user.js
+// @updateURL    https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/mb-reledit-release_rel_to_recording_rel.user.js
+// @supportURL   https://github.com/loujine/musicbrainz-scripts
+// @icon         https://raw.githubusercontent.com/loujine/musicbrainz-scripts/master/icon.png
+// @description  musicbrainz.org relation editor: Replace release relations by recording relations
+// @compatible   firefox+tampermonkey
+// @license      MIT
+// @require      https://gist.githubusercontent.com/reosarevok/e9fc05d7f251379c301b948623b3ef03/raw/e635364e2c3a60578bb349a9a95483711f6c4e4d/gistfile1.js
+// @include      http*://*musicbrainz.org/release/*/edit-relationships
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+function moveAR() {
+    const release = MB.relationshipEditor.state.entity;
+    const recordings = MB.tree.toArray(MB.relationshipEditor.state.selectedRecordings);
+    release.relationships.filter(
+        rel => Object.keys(server.releaseLinkTypeID).includes(String(rel.linkTypeID))
+    ).map(artistRel => {
+        recordings.map(rec => {
+            MB.relationshipEditor.dispatch({
+                type: 'update-relationship-state',
+                sourceEntity: rec,
+                ...relEditor.dispatchDefaults,
+                newRelationshipState: {
+                    ...relEditor.stateDefaults,
+                    _status: 1,
+                    attributes: relEditor.createAttributeTree(artistRel.attributes),
+                    begin_date: artistRel.begin_date,
+                    entity0: artistRel.target,
+                    entity0_credit: artistRel.entity0_credit,
+                    entity1: rec,
+                    end_date: artistRel.end_date,
+                    ended: artistRel.ended,
+                    id: MB.relationshipEditor.getRelationshipStateId(),
+                    linkTypeID: server.releaseToRecordingLink(artistRel.linkTypeID),
+                },
+            });
+        });
+        if (recordings.length) {
+            document.getElementById(
+                `remove-relationship-${artistRel.target_type}-` +
+                `${artistRel.source_type}-${artistRel.id}`
+            ).click();
+        }
+    });
+}
+
+(function displayToolbar() {
+    relEditor.container(document.querySelector('div.tabs'))
+             .insertAdjacentHTML('beforeend', `
+        <h3>Move release relations to recordings</h3>
+        <input type="button" id="moveAR" value="Move release relations to selected recordings">
+    `);
+})();
+
+$(document).ready(function () {
+    document.getElementById('moveAR').addEventListener('click', () => {
+        moveAR();
+        relEditor.editNote(
+            GM_info.script,
+            'Move performers in release relations to individual recordings'
+        );
+    });
+    return false;
+});
